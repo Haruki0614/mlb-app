1ee3862aeeeb4c87ca34a1652ad6ff43bf5f70e6
Haruki0614 <ac25307@gm.ibaraki-ct.ac.jp>
2025-09-26 20:12:44 +0900

    add/players-detail

--- backend/players-get.mjs
+++ backend/players-get.mjs
@@ -6,7 +6,9 @@ export const handler = async (event) => {
 
     for (const id of japanesePlayerIds) {
       const res = await fetch(`https://statsapi.mlb.com/api/v1/people/${id}`);
+      const playerStatsRes = await fetch(`https://statsapi.mlb.com/api/v1/people/${id}/stats?stats=season&group=pitching&season=2025`);
       const data = await res.json();
+      const playerStatsData = await playerStatsRes.json();
 
       if (!data.people || data.people.length === 0) continue;
 
@@ -14,7 +16,7 @@ export const handler = async (event) => {
       players.push({
         id: player.id,
         name: player.fullName,
-        
+        stats: playerStatsData.stats || []
       });
     }
 
